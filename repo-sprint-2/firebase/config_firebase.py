75a8ed46fc2b9b1ba917de132bad38f09399aa69
Juanpabloflorez <juflorezru@unal.edu.co>
2025-12-01 21:46:46 -0500

    Add files via upload

---
+++ repo-sprint-2/firebase/config_firebase.py
@@ -0,0 +1,23 @@
+import firebase_admin
+from firebase_admin import db, credentials
+
+
+class FirebaseConfig:
+    #Configuración Singleton de Firebase
+    _instance = None
+    _initialized = False
+    
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
+    
+    def initialize(self):
+        if not self._initialized:
+            cred = credentials.Certificate("repo-sprint-2/credentials.json")
+            firebase_admin.initialize_app(cred, {"databaseURL": "https://proyecto-poo-b05c4-default-rtdb.firebaseio.com/"})
+            self._initialized = True
+    
+    def get_reference(self, path: str = "/"):
+        return db.reference(path)
